1226015bdbcf006be1949cbd83c613e74f678ec8
Kazuya Takei <myself@attakei.net>
2025-03-29 22:06:01 +0900

    feat: Add load_path argument

--- src/sass_embedded/simple.py
+++ src/sass_embedded/simple.py
@@ -1,14 +1,22 @@
 """Simple interface using Dart Sass."""
 
+from __future__ import annotations
+
 import subprocess
-from typing import Literal
+from typing import Literal, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from pathlib import Path
+    from typing import Optional
 
 from .dart_sass import Release
 
 Syntax = Literal["scss", "sass", "css"]
 
 
-def compile_string(source: str, syntax: Syntax = "scss") -> str:
+def compile_string(
+    source: str, syntax: Syntax = "scss", load_paths: Optional[list[Path]] = None
+) -> str:
     """Convert from Sass/SCSS source to CSS."""
     exe = Release.init().get_executable()
     cmd = [
@@ -18,6 +26,8 @@ def compile_string(source: str, syntax: Syntax = "scss") -> str:
     ]
     if syntax == "sass":
         cmd.append("--indented")
+    if load_paths:
+        cmd += [f"--load-path={p}" for p in load_paths]
     proc = subprocess.run(
         cmd,
         input=source,
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -9,7 +9,7 @@ here = Path(__file__).parent
 targets = [
     css.stem
     for css in (here / "test-basics").glob("*.css")
-    if not css.name.startswith("_")
+    if not css.name.startswith("_") and css.stem not in ["modules"]
 ]
 
 
@@ -26,4 +26,34 @@ def test_compile_string_sass(target: str):
     source = here / "test-basics" / f"{target}.sass"
     expect = here / "test-basics" / f"{target}.css"
     result = M.compile_string(source.read_text(), syntax="sass")
+    assert result == expect.read_text()
+
+
+@pytest.mark.parametrize(
+    "source_path,load_dir",
+    [
+        ("modules-scss/styles.scss", "modules-scss"),
+        ("modules-scss/styles.scss", "modules-sass"),
+        ("modules-sass/styles.sass", "modules-scss"),
+        ("modules-sass/styles.sass", "modules-sass"),
+    ],
+)
+def test_compile_string_moduled_scss(source_path: str, load_dir: str):
+    source = here / "test-basics" / source_path
+    expect = here / "test-basics" / "modules.css"
+    module_dir = here / "test-basics" / load_dir
+    result = M.compile_string(
+        source.read_text(),
+        syntax=source.name[-4:],  # type: ignore[arg-type]
+        load_paths=[module_dir],
+    )
+    assert result == expect.read_text()
+
+
+def test_compile_string_moduled_sass():
+    source = here / "test-basics" / "modules-scss" / "styles.scss"
+    expect = here / "test-basics" / "modules.css"
+    result = M.compile_string(
+        source.read_text(), syntax="scss", load_paths=[source.parent]
+    )
     assert result == expect.read_text()
