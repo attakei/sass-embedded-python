7926ee20a3b7e2ba4b7df30c578ef19e123d2cd6
Kazuya Takei <myself@attakei.net>
2025-03-30 19:30:04 +0900

    refactor: Add document and comments

--- hatch_build.py
+++ hatch_build.py
@@ -1,5 +1,12 @@
+"""Custom hooks to build wheel file for platforms.
+
+This is hatchling build hook that add Dart Sass executables adapted specified platforms into bdist.
+
+To enable this, you should pass environment variables: ``BUILD_FOR_PLATFORM``
+that must be contains in keys of ``RELEASE_TARGET``.
+"""
+
 import os
-import sys
 import subprocess
 from pathlib import Path
 
@@ -18,15 +25,23 @@ here = Path(__file__).parent
 class CustomHook(BuildHookInterface):
     def initialize(self, version, build_data):
         if "BUILD_FOR_PLATFORM" not in os.environ:
+            # Skip hook when env is not passed.
             return
         if os.environ["BUILD_FOR_PLATFORM"] not in RELEASE_TARGET:
+            # Skip hook when env is not contained in targets.
+            # TODO: It should display warnings.
             return
         platform = os.environ["BUILD_FOR_PLATFORM"]
         release = RELEASE_TARGET[platform]
 
+        # Mark that bdist includes bineries for platform.
         build_data["pure_python"] = False
+
+        # Set build tag
         py_tag = "py3"
         abi_tag = "none"
         build_data["tag"] = f"{py_tag}-{abi_tag}-{platform}"
+
+        # Fetch Dart Sass executables for platform.
         cmd = f"python -m sass_embedded.dart_sass --clean --os {release['os']} --arch {release['arch']}"
         subprocess.run(cmd.split(), cwd=here / "src")
