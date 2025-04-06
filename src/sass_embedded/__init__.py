5852779b4f5f31a92ad8c26177453ed8152645fb
Kazuya Takei <myself@attakei.net>
2025-04-07 02:22:59 +0900

    refactor: Sort position of import

--- src/sass_embedded/__init__.py
+++ src/sass_embedded/__init__.py
@@ -4,8 +4,8 @@ __version__ = "0.0.0"
 
 from pathlib import Path
 
-package_root = Path(__file__).parent
-
 from .simple import compile_file, compile_string
+
+package_root = Path(__file__).parent
 
 __all__ = ["compile_file", "compile_string"]
