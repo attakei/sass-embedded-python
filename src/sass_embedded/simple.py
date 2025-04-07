0ee5d319b78eb1ba58d26ddc77438cee06d4e285
Kazuya Takei <myself@attakei.net>
2025-04-08 01:21:34 +0900

    refactor: Collect shared procs

--- src/sass_embedded/simple.py
+++ src/sass_embedded/simple.py
@@ -9,27 +9,47 @@ from typing import Literal, TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import Optional
 
-from .dart_sass import Release
+from .dart_sass import Executable, Release
 
 Syntax = Literal["scss", "sass", "css"]
+
+
+class CLI:
+    """CLI controls."""
+
+    exe: Executable
+    paths: list[Path]
+
+    def __init__(
+        self,
+        load_paths: Optional[list[Path]] = None,
+    ):
+        self.exe = Release.init().get_executable()
+        self.paths = load_paths or []
+
+    def _command_base(self) -> list[str]:
+        return [
+            str(self.exe.dart_vm_path),
+            str(self.exe.sass_snapshot_path),
+        ] + [f"--load-path={p}" for p in self.paths]
+
+    def command_with_path(self, source: Path, dest: Path) -> list[str]:
+        return self._command_base() + [f"{source}:{dest}"]
+
+    def command_with_stdin(self, syntax: Syntax) -> list[str]:
+        opts = ["--stdin"]
+        if syntax == "sass":
+            opts.append("--indented")
+        return self._command_base() + opts
 
 
 def compile_string(
     source: str, syntax: Syntax = "scss", load_paths: Optional[list[Path]] = None
 ) -> str:
     """Convert from Sass/SCSS source to CSS."""
-    exe = Release.init().get_executable()
-    cmd = [
-        str(exe.dart_vm_path),
-        str(exe.sass_snapshot_path),
-        "--stdin",
-    ]
-    if syntax == "sass":
-        cmd.append("--indented")
-    if load_paths:
-        cmd += [f"--load-path={p}" for p in load_paths]
+    cli = CLI(load_paths)
     proc = subprocess.run(
-        cmd,
+        cli.command_with_stdin(syntax),
         input=source,
         text=True,
         capture_output=True,
@@ -73,15 +93,10 @@ def compile_directory(
     See https://sass-lang.com/documentation/cli/dart-sass/#many-to-many-mode
     """
 
-    exe = Release.init().get_executable()
-    cmd = [
-        str(exe.dart_vm_path),
-        str(exe.sass_snapshot_path),
-        f"{source}:{dest}",
-    ]
-    if load_paths:
-        cmd += [f"--load-path={p}" for p in load_paths]
-    proc = subprocess.run(cmd, capture_output=True, text=True)
+    cli = CLI(load_paths)
+    proc = subprocess.run(
+        cli.command_with_path(source, dest), capture_output=True, text=True
+    )
     if proc.returncode != 0:
         raise Exception(proc.stdout + proc.stderr)
     return [p for p in Path(dest).glob("*.css")]
