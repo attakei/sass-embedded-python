21c506bfb1c9dba232538f71ae95a3850d9bc128
Kazuya Takei <myself@attakei.net>
2025-04-09 02:39:25 +0900

    ci: Refactor test resources
    - Add compressed files and rename expended files
    - More parametrize cases
    - Add pattern for style cases
    
    Refs: #1

--- tests/test_root.py
+++ tests/test_root.py
@@ -7,44 +7,31 @@ import sass_embedded as M
 here = Path(__file__).parent
 
 targets = [
-    css.stem
-    for css in (here / "test-basics").glob("*.css")
-    if not css.name.startswith("_") and css.stem not in ["modules"]
+    css.stem.split(".")[0]
+    for css in (here / "test-basics").glob("*.expanded.css")
+    if not css.name.startswith("_") and css.stem not in ["modules.expanded"]
 ]
 
 
 @pytest.mark.parametrize("target", targets)
-def test_compile_string_scss(target: str):
-    source = here / "test-basics" / f"{target}.scss"
-    expect = here / "test-basics" / f"{target}.css"
-    result = M.compile_string(source.read_text(), syntax="scss")
+@pytest.mark.parametrize("syntax", ["sass", "scss"])
+@pytest.mark.parametrize("style", ["expanded", "compressed"])
+def test_compile_string(target: str, syntax: str, style: str):
+    source = here / "test-basics" / f"{target}.{syntax}"
+    expect = here / "test-basics" / f"{target}.{style}.css"
+    result = M.compile_string(source.read_text(), syntax=syntax, style=style)  # type: ignore[arg-type]
     assert result == expect.read_text()
 
 
 @pytest.mark.parametrize("target", targets)
-def test_compile_string_sass(target: str):
-    source = here / "test-basics" / f"{target}.sass"
-    expect = here / "test-basics" / f"{target}.css"
-    result = M.compile_string(source.read_text(), syntax="sass")
-    assert result == expect.read_text()
-
-
-@pytest.mark.parametrize("target", targets)
-def test_compile_file_scss(target: str, tmpdir: Path):
-    source = here / "test-basics" / f"{target}.scss"
-    expect = here / "test-basics" / f"{target}.css"
+@pytest.mark.parametrize("syntax", ["sass", "scss"])
+@pytest.mark.parametrize("style", ["expanded", "compressed"])
+def test_compile_file(target: str, syntax: str, style: str, tmpdir: Path):
+    source = here / "test-basics" / f"{target}.{syntax}"
+    expect = here / "test-basics" / f"{target}.{style}.css"
     dest = tmpdir / f"{target}.css"
-    result = M.compile_file(source, dest)
-    assert expect.read_text() in result.read_text()
-
-
-@pytest.mark.parametrize("target", targets)
-def test_compile_file_sass(target: str, tmpdir: Path):
-    source = here / "test-basics" / f"{target}.sass"
-    expect = here / "test-basics" / f"{target}.css"
-    dest = tmpdir / f"{target}.css"
-    result = M.compile_file(source, dest)
-    assert expect.read_text() in result.read_text()
+    result = M.compile_file(source, dest, style=style)  # type: ignore[arg-type]
+    assert expect.read_text().strip() in result.read_text().strip()
 
 
 @pytest.mark.parametrize(
@@ -58,7 +45,7 @@ def test_compile_file_sass(target: str, tmpdir: Path):
 )
 def test_compile_string_moduled_scss(source_path: str, load_dir: str):
     source = here / "test-basics" / source_path
-    expect = here / "test-basics" / "modules.css"
+    expect = here / "test-basics" / "modules.expanded.css"
     module_dir = here / "test-basics" / load_dir
     result = M.compile_string(
         source.read_text(),
@@ -70,7 +57,7 @@ def test_compile_string_moduled_scss(source_path: str, load_dir: str):
 
 def test_compile_string_moduled_sass():
     source = here / "test-basics" / "modules-scss" / "styles.scss"
-    expect = here / "test-basics" / "modules.css"
+    expect = here / "test-basics" / "modules.expanded.css"
     result = M.compile_string(
         source.read_text(), syntax="scss", load_paths=[source.parent]
     )
