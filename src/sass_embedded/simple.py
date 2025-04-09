fa31d99fc8b8af13cb7eada2d6eaf9b39c004ad6
Kazuya Takei <myself@attakei.net>
2025-04-10 02:34:07 +0900

    faet: CompileOptions has attributes for source-map
    - It defines default value that keep behavior.
    - Checked to pass all tests.

--- src/sass_embedded/simple.py
+++ src/sass_embedded/simple.py
@@ -10,7 +10,7 @@ Finally, this provides all features of `Dart Sass CLI`_ excluded something.
 from __future__ import annotations
 
 import subprocess
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Literal, TYPE_CHECKING
 
@@ -21,19 +21,68 @@ from .dart_sass import Executable, Release
 
 Syntax = Literal["scss", "sass", "css"]
 OutputStyle = Literal["expanded", "compressed"]
+SourceMapStyle = Literal["refer", "embed"]
+SourceMapUrl = Literal["relative", "absolute"]
+
+
+@dataclass
+class SourceMapOptions:
+    """Source-map option values to compile for Dart Sass CLI."""
+
+    style: SourceMapStyle = "refer"
+    """Generating format for source-map.
+
+    :ref: https://sass-lang.com/documentation/cli/dart-sass/#embed-source-map
+    """
+    source_url: SourceMapUrl = "relative"
+    """Refer style for URL of source-map.
+
+    :ref: https://sass-lang.com/documentation/cli/dart-sass/#source-map-urls
+    """
+    source_embed: bool = False
+    """Flag to inject sources into sourcemap.
+
+    :ref: https://sass-lang.com/documentation/cli/dart-sass/#embed-sources
+    """
+
+    @property
+    def as_arguments(self) -> list[str]:
+        args = ["--source-map-urls=relative"]
+        return args
 
 
 @dataclass
 class CompileOptions:
-    paths: list[Path]
-    output_style: OutputStyle
+    """Compile option values for Dart Sass CLI."""
 
-    @property
-    def cli_arguments(self) -> list[str]:
-        """Arguments collection to pass CLI."""
-        return [
+    paths: list[Path] = field(default_factory=list)
+    """Path list of external modules.
+
+    :ref: https://sass-lang.com/documentation/cli/dart-sass/#load-path
+    """
+    output_style: OutputStyle = "expanded"
+    """Generating format for CSS.
+
+    :ref: https://sass-lang.com/documentation/cli/dart-sass/#style
+    """
+    sourcemap_options: Optional[SourceMapOptions] = None
+    """Generating options for source-map."""
+
+    def get_cli_arguments(self, use_stdout: bool = False) -> list[str]:
+        """Retrieve arguments collection to pass CLI.
+
+        :param use_stdout: Set True when CLI write compile files into STDOUT.
+        """
+        args = [
             f"--style={self.output_style}",
         ] + [f"--load-path={p}" for p in self.paths]
+        if use_stdout:
+            # When use stdout to write compiled CSS, it cannot use source-map options.
+            return args
+        # Work arguments for source-map.
+        if not self.sourcemap_options:
+            return args
+        return args + self.sourcemap_options.as_arguments
 
 
 class CLI:
@@ -50,16 +99,20 @@ class CLI:
         return [
             str(self.exe.dart_vm_path),
             str(self.exe.sass_snapshot_path),
-        ] + self.options.cli_arguments
+        ]
 
     def command_with_path(self, source: Path, dest: Path) -> list[str]:
-        return self._command_base() + [f"{source}:{dest}"]
+        return (
+            self._command_base()
+            + self.options.get_cli_arguments()
+            + [f"{source}:{dest}"]
+        )
 
     def command_with_stdin(self, syntax: Syntax) -> list[str]:
         opts = ["--stdin"]
         if syntax == "sass":
             opts.append("--indented")
-        return self._command_base() + opts
+        return self._command_base() + opts + self.options.get_cli_arguments(True)
 
 
 def compile_string(
