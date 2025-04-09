d39d67f5e23c0a584d6bb203095f652367bae1b4
Kazuya Takei <myself@attakei.net>
2025-04-10 02:15:15 +0900

    refactor: Collect compile options as class

--- src/sass_embedded/simple.py
+++ src/sass_embedded/simple.py
@@ -10,6 +10,7 @@ Finally, this provides all features of `Dart Sass CLI`_ excluded something.
 from __future__ import annotations
 
 import subprocess
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal, TYPE_CHECKING
 
@@ -22,28 +23,34 @@ Syntax = Literal["scss", "sass", "css"]
 OutputStyle = Literal["expanded", "compressed"]
 
 
+@dataclass
+class CompileOptions:
+    paths: list[Path]
+    output_style: OutputStyle
+
+    @property
+    def cli_arguments(self) -> list[str]:
+        """Arguments collection to pass CLI."""
+        return [
+            f"--style={self.output_style}",
+        ] + [f"--load-path={p}" for p in self.paths]
+
+
 class CLI:
     """CLI controls."""
 
     exe: Executable
-    paths: list[Path]
-    style: OutputStyle
+    options: CompileOptions
 
-    def __init__(
-        self,
-        load_paths: Optional[list[Path]] = None,
-        output_style: OutputStyle = "expanded",
-    ):
+    def __init__(self, options: CompileOptions):
         self.exe = Release.init().get_executable()
-        self.paths = load_paths or []
-        self.style = output_style
+        self.options = options
 
     def _command_base(self) -> list[str]:
         return [
             str(self.exe.dart_vm_path),
             str(self.exe.sass_snapshot_path),
-            f"--style={self.style}",
-        ] + [f"--load-path={p}" for p in self.paths]
+        ] + self.options.cli_arguments
 
     def command_with_path(self, source: Path, dest: Path) -> list[str]:
         return self._command_base() + [f"{source}:{dest}"]
@@ -68,7 +75,8 @@ def compile_string(
     :param load_paths: List of addtional load path for Sass compile.
     :param style: Output style.
     """
-    cli = CLI(load_paths, style)
+    options = CompileOptions(load_paths or [], style)
+    cli = CLI(options)
     proc = subprocess.run(
         cli.command_with_stdin(syntax),
         input=source,
@@ -91,7 +99,8 @@ def compile_file(
     :param load_paths: List of addtional load path for Sass compile.
     :param style: Output style.
     """
-    cli = CLI(load_paths, style)
+    options = CompileOptions(load_paths or [], style)
+    cli = CLI(options)
     proc = subprocess.run(
         cli.command_with_path(source, dest), capture_output=True, text=True
     )
@@ -118,7 +127,8 @@ def compile_directory(
     :param style: Output style.
     """
 
-    cli = CLI(load_paths, style)
+    options = CompileOptions(load_paths or [], style)
+    cli = CLI(options)
     proc = subprocess.run(
         cli.command_with_path(source, dest), capture_output=True, text=True
     )
