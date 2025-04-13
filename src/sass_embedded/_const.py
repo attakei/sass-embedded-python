9f91b96fb63be0f9e2e0e702bab614f157720b78
Kazuya Takei <myself@attakei.net>
2025-04-13 23:36:19 +0900

    feat: Declare about The Embedded Sass Protocol

--- src/sass_embedded/_const.py
+++ src/sass_embedded/_const.py
@@ -2,3 +2,6 @@
 
 DART_SASS_VERSION = "1.86.3"
 """Version of bundled Dart Sass."""
+
+EMBEDDED_PROTOCOL_VERSION = "3.1.0"
+"""Version of The Embedded Sass Protocol."""
---
+++ src/sass_embedded/protocol/__init__.py
@@ -0,0 +1,6 @@
+"""Interface of The Embedded Sass Protocol.
+
+This package manages components to handle `The Embedded Sass Protocol`_.
+
+.. _The Embedded Sass Protocol: https://github.com/sass/sass/blob/main/spec/embedded-protocol.md
+"""
