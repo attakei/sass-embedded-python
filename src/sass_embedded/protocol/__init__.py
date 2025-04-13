948cbf15eccde4d83e358fa8efb3d944236efca0
Kazuya Takei <myself@attakei.net>
2025-04-14 02:57:22 +0900

    feat: Low-leve protobuf communicate to Dart Sass host
    - It doesn't write tests.
    - It shows example to use protocol instead of tests
      (example runs correctly).

--- src/sass_embedded/protocol/__init__.py
+++ src/sass_embedded/protocol/__init__.py
@@ -2,5 +2,10 @@
 
 This package manages components to handle `The Embedded Sass Protocol`_.
 
+.. important::
+
+   This is first demo feature.
+   It will work correctly, but user must handle raw protobuf objects.
+
 .. _The Embedded Sass Protocol: https://github.com/sass/sass/blob/main/spec/embedded-protocol.md
 """
