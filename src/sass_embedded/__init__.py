a96b3ed6da4ff18e96722830b03706d04cba7d2c
Martin Heubuch <martin.heubuch@spie-isw.com>
2025-12-05 11:24:34 +0100

    feat: Uncomment Compiler import and update __all__ to include all Sass types

--- src/sass_embedded/__init__.py
+++ src/sass_embedded/__init__.py
@@ -3,6 +3,7 @@
 __version__ = "0.1.3"
 
 from .simple import compile_directory, compile_file, compile_string
-# from .protocol.compiler import Compiler  # TODO: Uncomment when Compiler is complete
+from .protocol.compiler import Compiler
+from .protocol.sass_function import SassFunction, SassBoolean, SassColor, SassList, SassMap, SassNumber, SassString, SassValue
 
-__all__ = ["compile_directory", "compile_file", "compile_string"]  # , "Compiler"]
+__all__ = ["compile_directory", "compile_file", "compile_string", "Compiler", "SassFunction", "SassBoolean", "SassColor", "SassList", "SassMap", "SassNumber", "SassString", "SassValue"]
